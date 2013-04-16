69aeadf578d61e0bc830595d7d0151cc9483343c
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-16 21:59:29 +0200

    initial src bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,15 +1,13 @@
 <?php
 /**
- * test bootstrap
+ * bootstrap
  *
  * PHP Version 5
  *
- * @category  Application
+ * @category  Bootstrap
  * @package   Test
  * @author    Lucas S. Bickel <hairmare@purplehaze.ch>
  * @copyright 2013 - Alle Rechte vorbehalten
  * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
  * @link      http://hairmare.ch
  */
-
-require_once __DIR__.'/../src/Bootstrap.php';
