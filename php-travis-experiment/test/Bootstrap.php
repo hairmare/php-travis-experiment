00955e6d11e211aa909ef7c77a0210b526c3aaaf
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-16 21:26:39 +0200

    add test bootstrap

---
+++ php-travis-experiment/test/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * test bootstrap
+ *
+ * PHP Version 5
+ *
+ * @category  Application
+ * @package   Test
+ * @author    Lucas S. Bickel <hairmare@purplehaze.ch>
+ * @copyright 2013 - Alle Rechte vorbehalten
+ * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link      http://hairmare.ch
+ */
+
+require_once __DIR__.'/../src/Bootstrap.php';
